c8832422345f4caf884384ad6b81866bdac0df07
Scott Stephens <stephens.js@gmail.com>
2018-04-03 12:49:09 -0500

    Add config file to console project

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,10 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"1 + 2 = {Adder.Add(1,2)}");
+            var num1 = Int32.Parse(ConfigurationManager.AppSettings["num1"]);
+            var num2 = Int32.Parse(ConfigurationManager.AppSettings["num2"]);
+
+            Console.WriteLine($"{num1} + {num2} = {Adder.Add(num1,num2)}");
 
             var now = SystemClock.Instance.GetCurrentInstant();
             var now_local = now.InZone(DateTimeZoneProviders.Tzdb.GetSystemDefault());
